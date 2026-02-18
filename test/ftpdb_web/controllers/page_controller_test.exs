de490e9d5bd5912c18c22820ddccac09143a9cac
jam06452 <jam@jam06452.uk>
2026-02-18 19:43:29 +0000

    Fixed bug where when you clicked on a project it brought the user to the /project/:user_id

--- test/ftpdb_web/controllers/page_controller_test.exs
+++ test/ftpdb_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule FtpdbWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "FTPDB"
   end
 end
