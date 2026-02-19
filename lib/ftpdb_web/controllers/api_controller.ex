0626142bc0f86b19cfbf31483b93a8e219c27754
jam06452 <jam@jam06452.uk>
2026-02-19 21:50:38 +0000

    Chores on api controller

--- lib/ftpdb_web/controllers/api_controller.ex
+++ lib/ftpdb_web/controllers/api_controller.ex
@@ -1,19 +1,19 @@
 defmodule FtpdbWeb.ApiController do
   use FtpdbWeb, :controller
 
-  def hot(conn, _params) do
+  def hot do
     hot = Cachex.fetch!(:hot_cache, "hot", fn _key -> Ftpdb.DB.hot() end)
     json(conn, hot)
   end
 
-  def top_this_week(conn, _params) do
+  def top_this_week do
     top_this_week =
       Cachex.fetch!(:top_week_cache, "top_this_week", fn _key -> Ftpdb.DB.top_this_week() end)
 
     json(conn, top_this_week)
   end
 
-  def fan_favourites(conn, _params) do
+  def fan_favourites do
     fan_favourites =
       Cachex.fetch!(:fan_favourites_cache, "fan_favourites", fn _key ->
         Ftpdb.DB.fan_favourites()
@@ -22,14 +22,14 @@ defmodule FtpdbWeb.ApiController do
     json(conn, fan_favourites)
   end
 
-  def top_all_time(conn, _params) do
+  def top_all_time do
     top_all_time =
       Cachex.fetch!(:top_all_time_cache, "top_all_time", fn _key -> Ftpdb.DB.top_all_time() end)
 
     json(conn, top_all_time)
   end
 
-  def most_time_spent(conn, _params) do
+  def most_time_spent do
     most_time_spent =
       Cachex.fetch!(:most_time_spent_cache, "most_time_spent", fn _key ->
         Ftpdb.DB.most_time_spent()
@@ -38,7 +38,7 @@ defmodule FtpdbWeb.ApiController do
     json(conn, most_time_spent)
   end
 
-  def devlogs(conn, %{"id" => id}) do
+  def devlogs(id) do
     devlog =
       Cachex.fetch!(:devlog_cache, id, fn _key -> Ftpdb.DB.get_devlogs(id) end,
         expiration: :timer.minutes(60)
@@ -47,7 +47,7 @@ defmodule FtpdbWeb.ApiController do
     json(conn, devlog)
   end
 
-  def project_info(conn, %{"id" => id}) do
+  def project_info(id) do
     project_info =
       Cachex.fetch!(:project_cache, id, fn _key -> Ftpdb.DB.get_project_info(id) end,
         expiration: :timer.minutes(60)
@@ -56,7 +56,7 @@ defmodule FtpdbWeb.ApiController do
     json(conn, project_info)
   end
 
-  def user_info(conn, %{"id" => id}) do
+  def user_info(id) do
     id = to_string(id)
 
     user_info =
@@ -67,7 +67,7 @@ defmodule FtpdbWeb.ApiController do
     json(conn, List.first(user_info))
   end
 
-  def search(conn, %{"q" => query} = params) do
+  def search(query) do
     min_hours =
       case params["min_hours"] do
         nil ->
@@ -86,10 +86,6 @@ defmodule FtpdbWeb.ApiController do
     })
   end
 
-  def search(conn, _params) do
-    json(conn, %{projects: [], users: []})
-  end
-
   def suggest(conn, %{
         "category" => category,
         "display_name" => display_name,
@@ -100,11 +96,23 @@ defmodule FtpdbWeb.ApiController do
   end
 
   def random_projects(conn, %{"filter" => filter}) do
-    json(conn, Ftpdb.DB.random_projects(filter))
+    case filter do
+      "stat_hot_score" ->
+        hot(conn, %{})
+
+      "stat_total_likes" ->
+        fan_favourites(conn, %{})
+
+      "stat_total_duration_seconds" ->
+        most_time_spent(conn, %{})
+
+      _ ->
+        json(conn, Ftpdb.DB.random_projects())
+    end
   end
 
   def random_projects(conn, _params) do
-    json(conn, Ftpdb.DB.random_projects("random"))
+    json(conn, Ftpdb.DB.random_projects())
   end
 
   def user_projects(conn, %{"user_id" => user_id}) do
